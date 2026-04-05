11229daca9ffaf214d733a3f4727f4d11d250a1a
Aadarsh Srivastava <aadarshaaryan84@gmail.com>
2026-04-05 21:40:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 let player = document.getElementById('player');
-
+let obstacle = document.getElementById('obstacle');
+let btn = document.getElementById('btn');
+let timeLeft = 0;
+let timerInterval;
 let jumping = false;
-function jump() {
-    if (jumping) return;
 
-    jumping = true;
-    player.classList.add('jump');
+btn.style.display = 'block'
 
-    setTimeout(() => {
-        player.classList.remove('jump');
-        jumping = false;
-    }, 1200);
-}
+btn.addEventListener('click', () => {
+    wrapper();
+    btn.style.display = 'none'
+})
 
-document.addEventListener('keydown', (e) => {
-    if (e.code === "ArrowUp") {
+function wrapper() {
+    obstacle.classList.add('move')
+    function jump() {
+        if (jumping) return;
+
+        jumping = true;
+        player.classList.add('jump');
+
+        setTimeout(() => {
+            player.classList.remove('jump');
+            jumping = false;
+        }, 1200);
+    }
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "ArrowUp") {
+            jump();
+        }
+    });
+
+    setInterval(() => {
+        let playerBottom = parseInt(window.getComputedStyle(player).getPropertyValue('bottom'));
+        let obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue('left'));
+
+        if (obstacleLeft < 200 && obstacleLeft > 170 && playerBottom < 50) {
+            alert(`Score: ${timeLeft} | Game Over 💀 | click OK to restart`);
+        }
+    }, 10);
+
+    document.body.addEventListener('pointerdown', () => {
         jump();
-    }
-});
+    });
 
-let obstacle = document.querySelector('.obstacle');
-
-setInterval(() => {
-    let playerBottom = parseInt(window.getComputedStyle(player).getPropertyValue('bottom'));
-    let obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue('left'));
-
-    if (obstacleLeft < 200 && obstacleLeft > 170 && playerBottom < 50) {
-        alert("Game Over 💀 | click OK to restart");
-    }
-}, 10);
-
-document.body.addEventListener('pointerdown', () => {
-    jump();
-});
+    timerInterval = setInterval(() => {
+        timeLeft++;
+        let p3 = document.getElementById('p3');
+        p3.innerHTML = `Score: ${timeLeft}`
+    }, 200)
+}

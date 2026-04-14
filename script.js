65b7679c75e7bd02844fcd0fda417bc5f8f792a2
Aadarsh Srivastava <aadarshaaryan84@gmail.com>
2026-04-14 11:19:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let player = document.getElementById('player');
 let obstacle = document.getElementById('obstacle');
 let btn = document.getElementById('btn');
+let p4 = document.getElementById('p4');
+let sky = document.querySelector('.gameArea');
+let speed = 2;
 let timeLeft = 0;
 let timerInterval;
 let jumping = false;
@@ -10,10 +13,16 @@ btn.style.display = 'block'
 btn.addEventListener('click', () => {
     wrapper();
     btn.style.display = 'none'
+    p4.style.visibility = 'hidden'
 })
 
 function wrapper() {
-    obstacle.classList.add('move')
+    sky.style.animation = `bgMove ${speed}s linear infinite`
+
+    obstacle.classList.add('move');
+    let moveSpeed = document.querySelector('.move');
+    moveSpeed.style.animation = `move ${speed}s linear infinite`;
+
     function jump() {
         if (jumping) return;
 
@@ -37,13 +46,21 @@ function wrapper() {
         let obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue('left'));
 
         if (obstacleLeft < 200 && obstacleLeft > 170 && playerBottom < 50) {
-            alert(`Score: ${timeLeft} | Game Over 💀 | click OK to restart`);
+            clearInterval(timerInterval);
+            alert(`Score: ${timeLeft} | Game Over 💀 | click OK to continue`);
+            location.reload();
         }
+
+        if (timeLeft >= 250) {
+            obstacle.style.animationDuration = '1.5s'
+        }
+
     }, 10);
 
     document.body.addEventListener('pointerdown', () => {
         jump();
     });
+
 
     timerInterval = setInterval(() => {
         timeLeft++;
@@ -51,3 +68,17 @@ function wrapper() {
         p3.innerHTML = `Score: ${timeLeft}`
     }, 200)
 }
+
+let buttons = document.querySelectorAll(".sp");
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+
+        buttons.forEach(b => b.classList.remove("active"));
+
+        btn.classList.add("active");
+    });
+    btn.addEventListener('click',()=>{
+        speed = btn.innerHTML;
+    })
+});
